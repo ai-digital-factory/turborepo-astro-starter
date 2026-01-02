9dda3028e4d15db8985274dfee6920c6b684540a
piyush arora <piyusharorawork@gmail.com>
2026-01-02 21:24:25 +0530

    Integrate React into Astro blog, update dependencies, and refactor Welcome component. Added background SVG and modified TypeScript configuration for JSX support.

---
+++ apps/blog/src/components/Welcome.tsx
@@ -0,0 +1,15 @@
+export default function Welcome() {
+  return (
+    <div id="container">
+      <main>
+        <h1>DIGITAL FACTORY</h1>
+        <img
+          src="/background.svg"
+          width="115"
+          height="48"
+          alt="Astro Homepage"
+        />
+      </main>
+    </div>
+  );
+}
